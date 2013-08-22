9f6e6bc4c414b976465796380309c76503cc9cf8
Mart·L <L@Ls-MBP.local>
2013-08-22 08:15:16 -0400

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by Mart·L on 8/22/13.
+//  Copyright (c) 2013 Martreen L. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
